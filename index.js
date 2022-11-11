4420a85b53eb6266faabd7fb01427969f49066e1
NecessaryPaint <60275363+NecessaryPaint@users.noreply.github.com>
2022-11-11 18:32:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ async function init(token) {
     // Register the stupid ass command
     stupidAssBot.on('ready', async () => {
         await stupidAssBot.bulkEditCommands([{
-            name: 'joe',
-            description: 'i cant believe youve done this',
+            name: 'lol',
+            description: 'I hate discord so much you cannot believe it',
             type: 1,
         }])
         console.log(`Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
     })
     // Stupid ass interaction creation event
     stupidAssBot.on('interactionCreate', async (interaction) => {
-        if (interaction?.data?.name === 'joe') {
+        if (interaction?.data?.name === 'lol') {
             await interaction.createMessage({
-                content: 'joe who? JOE MAMAM XD'
+                content: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway, because bees don\'t care what humans think is impossible.'
             })
             console.log('Self destructing...')
             process.exit(0)
