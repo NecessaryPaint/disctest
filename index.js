96c1a3228dd8f966f0ef70d2a88def522ed6f05c
NecessaryPaint <60275363+NecessaryPaint@users.noreply.github.com>
2022-11-11 18:17:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const { CommandClient } = require('eris')
+
+// Stupid ass bot creation
+async function init(token) {
+    const stupidAssBot = new CommandClient(`Bot ${token}`, { intents: ['guilds'], maxShards: 'auto',restMode: true })
+    // Register the stupid ass command
+    stupidAssBot.on('ready', async () => {
+        await stupidAssBot.bulkEditCommands([{
+            name: 'joe',
+            description: 'i cant believe youve done this',
+            type: 1,
+        }])
+        console.log(`Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
+    })
+    // Stupid ass interaction creation event
+    stupidAssBot.on('interactionCreate', async (interaction) => {
+        if (interaction?.data?.name === 'joe') {
+            await interaction.createMessage({
+                content: 'joe who? JOE MAMAM XD'
+            })
+            console.log('Self destructing...')
+            process.exit(0)
+        }
+    })
+    stupidAssBot.connect();
+}
+
+const tokenFromStupidCommand = process.argv[2]
+init(tokenFromStupidCommand);
